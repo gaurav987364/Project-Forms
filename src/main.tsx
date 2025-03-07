38fb1645c72d7e18263c07acf79d501185b87b6c
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-03-07 18:35:27 +0530

    bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import './index.css'
-import App from './App.tsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import './index.css';
+import App from './App.tsx';
 
 const queryClient = new QueryClient;
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
-    <App />
+      <App />
     <ReactQueryDevtools />
   </StrictMode>
   </QueryClientProvider>,
