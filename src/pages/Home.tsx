df6c568416d8395b1c279faaab9ce16e3691dcc2
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-03-18 17:57:30 +0530

    bug fix in css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,9 +8,9 @@ const Home = () => {
     navlink("/formlayout/info");
   };
   return (
-    <div className="relative w-full h-screen bg-slate-950 text-neutral-50">
+    <div className="relative w-full h-screen bg-gradient-to-bl from-[#0f172a] via-[#1e1a78] to-[#0f172a] text-neutral-50">
         <div className=" w-2/3 mx-auto h-full flex flex-col items-center justify-center space-y-5">
-            <h1 className=" text-center w-fit text-2xl font-mono capitalize"><strong className=" text-pink-500">Welcome</strong>😊,this is the demo of our production level multistep form.You can checkout form by clicking below button.</h1>
+            <h1 className=" text-center w-fit text-3xl font-mono capitalize bg-gradient-to-r from-yellow-500 via-red-500 to-pink-500 text-transparent bg-clip-text"><strong className=" text-pink-500">Welcome</strong>😊,this is the demo of our production level multistep form.You can checkout form by clicking below button.</h1>
             <Button 
               className=" cursor-pointer" 
               variant="outline" 
